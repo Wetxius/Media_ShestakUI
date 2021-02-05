d06b0046fb10f5559ec03144159f5ced2f62ce24
Wetxius <wetxius@gmail.com>
2021-02-05 13:16:43 +0300

    Added new fonts: Gotham and Supermolot.

--- Media_ShestakUI/Media.lua
+++ Media_ShestakUI/Media.lua
@@ -1,4 +1,4 @@
-local LSM = LibStub('LibSharedMedia-3.0')
+local LSM = LibStub("LibSharedMedia-3.0")
 
 local LOCALE_MASK = 0
 if GetLocale() == "koKR" then
@@ -13,21 +13,23 @@ else
 	LOCALE_MASK = 128
 end
 
-LSM:Register('font', 'Expressway', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Expressway.ttf]], LOCALE_MASK)
-LSM:Register('font', 'Homespun [Pixel]', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Homespun.ttf]], LOCALE_MASK)
-LSM:Register('font', 'Impacted', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Impacted.ttf]], LOCALE_MASK)
-LSM:Register('font', 'MagistralTT', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\MagistralTT.ttf]], LOCALE_MASK)
-LSM:Register('font', 'PT Sans Narrow', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\PTSansNarrow.ttf]], LOCALE_MASK)
-LSM:Register('font', 'Roboto Condensed', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\RobotoCondensed.ttf]], LOCALE_MASK)
-LSM:Register('font', 'Visitor Rus [Pixel]', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Visitor Rus.ttf]], LOCALE_MASK)
-LSM:Register('font', 'Vox', [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Vox.ttf]], LOCALE_MASK)
+LSM:Register("font", "Expressway", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Expressway.ttf]], LOCALE_MASK)
+LSM:Register("font", "Gotham", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Gotham.ttf]], LOCALE_MASK)
+LSM:Register("font", "Homespun [Pixel]", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Homespun.ttf]], LOCALE_MASK)
+LSM:Register("font", "Impacted", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Impacted.ttf]], LOCALE_MASK)
+LSM:Register("font", "MagistralTT", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\MagistralTT.ttf]], LOCALE_MASK)
+LSM:Register("font", "PT Sans Narrow", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\PTSansNarrow.ttf]], LOCALE_MASK)
+LSM:Register("font", "Roboto Condensed", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\RobotoCondensed.ttf]], LOCALE_MASK)
+LSM:Register("font", "Supermolot", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Supermolot.ttf]], LOCALE_MASK)
+LSM:Register("font", "Visitor Rus [Pixel]", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Visitor Rus.ttf]], LOCALE_MASK)
+LSM:Register("font", "Vox", [[Interface\AddOns\Media_ShestakUI\Media\Fonts\Vox.ttf]], LOCALE_MASK)
 
-LSM:Register('statusbar', 'Asphyxia', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Asphyxia]])
-LSM:Register('statusbar', 'Bezo', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Bezo]])
-LSM:Register('statusbar', 'Dajova', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Dajova]])
-LSM:Register('statusbar', 'Duffed', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Duffed]])
-LSM:Register('statusbar', 'ElvUI Gloss', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\ElvUI]])
-LSM:Register('statusbar', 'Sinaris', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Sinaris]])
-LSM:Register('statusbar', 'Slate', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Slate]])
-LSM:Register('statusbar', 'Smooth', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Smooth]])
-LSM:Register('statusbar', 'Tukui', [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Tukui]])
+LSM:Register("statusbar", "Asphyxia", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Asphyxia]])
+LSM:Register("statusbar", "Bezo", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Bezo]])
+LSM:Register("statusbar", "Dajova", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Dajova]])
+LSM:Register("statusbar", "Duffed", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Duffed]])
+LSM:Register("statusbar", "ElvUI Gloss", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\ElvUI]])
+LSM:Register("statusbar", "Sinaris", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Sinaris]])
+LSM:Register("statusbar", "Slate", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Slate]])
+LSM:Register("statusbar", "Smooth", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Smooth]])
+LSM:Register("statusbar", "Tukui", [[Interface\AddOns\Media_ShestakUI\Media\StatusBars\Tukui]])
